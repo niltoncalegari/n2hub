3641d4858d0795925336dc6334e4c6d614ebb6b3
Nilton Calegari <calegari.lk@gmail.com>
2025-01-10 21:44:02 -0300

    Enhance Next.js configuration with security headers and redirects
    - Enabled React strict mode for improved performance and debugging.
    - Added Strict-Transport-Security header to enhance security.
    - Implemented redirects for specific host to a new destination, improving user navigation.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'battlebit.wiki.gg',
-      'img.icons8.com'
-    ],
+  reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+        ],
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'seu-app.herokuapp.com',
+          },
+        ],
+        destination: 'https://www.seudominio.com/:path*',
+        permanent: true,
+      },
+    ]
   },
 }
 
